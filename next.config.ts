5aad5b6c77d750c008481ede409d95e66a648fbe
AravindanR-git <aravindan017@gmail.com>
2025-11-03 10:46:55 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/<repo-name>',     // <-- replace with your repo name
+  assetPrefix: '/<repo-name>/', // <-- replace with your repo name
 };
 
 export default nextConfig;
